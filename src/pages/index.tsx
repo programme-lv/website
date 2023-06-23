78bf1e50b8bada3c013e842d840ced528ad02ea0
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-23 15:48:41 +0300

    move inter font to app level

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 	return (
-		<main className={`p-5 ${inter.className}`}>
+		<main className='p-5'>
 			<NavBar />
 		</main>
 	)
