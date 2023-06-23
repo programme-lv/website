2595151bc5d857b87c23d488bd61204b563f9d41
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-23 15:39:51 +0300

    change spaces to tabs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import NavBar from '@/components/NavBar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className={`p-5 ${inter.className}`}>
-      <NavBar />
-    </main>
-  )
+	return (
+		<main className={`p-5 ${inter.className}`}>
+			<NavBar />
+		</main>
+	)
 }
---
+++ src/pages/tasks.tsx
@@ -0,0 +1,9 @@
+import NavBar from '@/components/NavBar'
+
+export default function Tasks() {
+	return (
+		<main className='p-5'>
+			<NavBar />
+		</main>
+	)
+}
