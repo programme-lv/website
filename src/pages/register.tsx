4a246880eaa3bd956b309a9404e0032b5c01ad51
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-25 19:19:57 +0300

    display error during registration

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -22,8 +22,8 @@ export default function Register() {
 
 
 const REGISTER_MUTATION = gql`
-  mutation Register($username: String!, $password: String!) {
-    register(username: $username, password: $password) {
+  mutation Register($username: String!, $password: String!, $email: String!, $firstName: String!, $lastName: String!) {
+    register(username: $username, password: $password, email: $email, firstName: $firstName, lastName: $lastName) {
       id
       username
     }
@@ -35,29 +35,58 @@ function RegistrationForm() {
     const [register, { data }] = useMutation(REGISTER_MUTATION, { client: apolloClient });
 
     const [username, setUsername] = useState<string>('')
-    const [firstname, setFirstName] = useState<string>('')
-    const [lastname, setLastName] = useState<string>('')
+    const [firstName, setFirstName] = useState<string>('')
+    const [lastName, setLastName] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
     const [repeatPassword, setRepeatPassword] = useState<string>('')
 
-    function handleRegistration(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+    const [error, setError] = useState<string>('')
+    const [success, setSuccess] = useState<boolean>(true);
 
-        register({ variables: { username, password } })
+    async function handleRegistration(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if (password !== repeatPassword) {
+            setError('Paroles nesakrīt')
+            return
+        }
+
+        try {
+            await register({ variables: { username, password, email, firstName, lastName } })
+        } catch (error: any) {
+            if (error.message) {
+                setError(error.message)
+            } else {
+                setError('Nezināma kļūda')
+            }
+            return;
+        }
+
+        setSuccess(true);
+    }
+
+    if (success) {
+        return (<div>
+            Reģistrācija veiksmīga!
+            <br></br>
+            <Link href="/login">Pieslēgties</Link>
+        </div>);
     }
 
     return (
         <form className='flex flex-col' onSubmit={handleRegistration}>
             <UsernameInput username={username} setUsername={setUsername} />
             <EmailInput email={email} setEmail={setEmail} />
-            <FirstNameInput firstname={firstname} setFirstName={setFirstName} />
-            <LastNameInput lastname={lastname} setLastName={setLastName} />
+            <FirstNameInput firstname={firstName} setFirstName={setFirstName} />
+            <LastNameInput lastname={lastName} setLastName={setLastName} />
             <PasswordInput password={password} setPassword={setPassword} />
             <RepeatPasswordInput password={repeatPassword} setPassword={setRepeatPassword} />
             <button type="submit">Reģistrēties</button>
             <div>
                 Jau esi piereģistrējies? <Link href="/login">Pieslēgties</Link>
+            </div>
+            <div>
+                {error}
             </div>
         </form>
     )
