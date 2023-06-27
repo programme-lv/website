9dd8dfd55cb03d2f0b11c2b880229440ec5a5c2f
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-27 19:52:29 +0300

    make the editor a bit better

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -13,31 +13,43 @@ export default function Editor() {
         alert(code);
     }
 
+    function handleEditorKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+        if (e.key === 'Tab') {
+            e.preventDefault();
+            const { selectionStart, selectionEnd, value } = e.currentTarget;
+            const newValue = value.substring(0, selectionStart) + '\t' + value.substring(selectionEnd);
+            setCode(newValue);
+            e.currentTarget.selectionStart = e.currentTarget.selectionEnd = selectionStart + 1;
+        }
+    }
+    
     return (
         <main className='p-5'>
             <NavBar />
-            <br />
-            <br />
-            <div className="w-[600px]">
-                tavs kods:
-                <textarea className='w-full h-[200px]' value={code} onChange={e => setCode(e.target.value)}></textarea>
-            </div>
-            <div className="w-[600px]">
-                ievaddati:
-                <textarea className='w-full h-[100px]' value={stdin} onChange={e => setCode(e.target.value)}></textarea>
-            </div>
-            <br />
-            <button onClick={executeCode}>izpildīt</button>
-            <br />
-            <br />
-            <div className="flex w-full gap-x-10 max-w-[600px]">
-                <div className="flex-grow">
-                    izpildes stdout:
-                    <textarea className='w-full h-[100px]' value={stdout} readOnly onChange={e => setStdout(e.target.value)}></textarea>
+
+            <div className="flex flex-col w-[600px] m-auto gap-3 mt-12">
+
+                <div className="w-full flex flex-col">
+                    tavs kods:
+                    <textarea className='w-full h-[300px]' onKeyDown={handleEditorKeyDown} style={{resize:'vertical'}} value={code} onChange={e => setCode(e.target.value)}></textarea>
                 </div>
-                <div className="flex-grow">
-                    izpildes stderr:
-                    <textarea className='w-full h-[100px]' value={stderr} readOnly onChange={e => setStderr(e.target.value)}></textarea>
+
+                <div className="w-full flex flex-col">
+                    ievaddati:
+                    <textarea className='w-full h-[100px]' style={{resize:'vertical'}} value={stdin} onChange={e => setStdin(e.target.value)}></textarea>
+                </div>
+
+                <button onClick={executeCode} className="my-2">izpildīt</button>
+
+                <div className="flex w-full gap-x-10 content-between">
+                    <div className="flex-grow-[1] flex flex-col">
+                        izpildes stdout:
+                        <textarea className='w-full h-[100px]' style={{resize:'vertical'}} value={stdout} readOnly onChange={e => setStdout(e.target.value)}></textarea>
+                    </div>
+                    <div className="flex-grow-[0.5] flex flex-col">
+                        izpildes stderr:
+                        <textarea className='w-full h-[100px]' style={{resize:'vertical'}} value={stderr} readOnly onChange={e => setStderr(e.target.value)}></textarea>
+                    </div>
                 </div>
             </div>
         </main>
