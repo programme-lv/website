4db33ca87b214d1dfb2e770cb057bc4a5426d95d
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-03 17:50:48 +0300

    mark programme.lv as active in navigation bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import NavigationBar from '@/components/NavigationBar'
 export default function Home() {
 	return (
 		<>
-			<NavigationBar />
+			<NavigationBar active='index'/>
 			<main>
 				<h1>Programme.lv</h1>
 			</main>
