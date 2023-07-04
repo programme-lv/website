6f8e209fc7f09ef864ddd9b8e9e1712ce6f480ef
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-04 15:25:30 +0300

    better login button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,22 +8,24 @@ import { useUser } from '@/contexts/UserContext'
 import { useRouter } from 'next/router'
 import useTranslation from 'next-translate/useTranslation'
 import NavigationBar from '@/components/NavigationBar';
-import { Button, Card } from '@mui/material';
+import { Alert, Card } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { Container } from '@mui/system';
+import LoginIcon from '@mui/icons-material/Login';
 
 export default function Login() {
-	return (
-		<div>
-			<NavigationBar active='login' />
-			<main className="container m-auto">
+    return (
+        <div>
+            <NavigationBar active='login' />
+            <main className="container m-auto">
                 <Container maxWidth='sm'>
-                <Card className='my-5 p-5' variant='outlined'>
-				<LoginForm />
-                </Card>
+                    <Card className='my-5 p-5' variant='outlined'>
+                        <LoginForm />
+                    </Card>
                 </Container>
-			</main>
-		</div>
-	)
+            </main>
+        </div>
+    )
 }
 
 const LOGIN_MUTATION = gql`
@@ -36,44 +38,56 @@ mutation Login($username: String!, $password: String!) {
 `;
 
 function LoginForm() {
-	const { t } = useTranslation('errors')
-	const { refreshUser } = useUser();
-	const [login] = useMutation(LOGIN_MUTATION, { client: apolloClient });
-	const router = useRouter();
+    const { t } = useTranslation('errors')
+    const { refreshUser } = useUser();
+    const [login] = useMutation(LOGIN_MUTATION, { client: apolloClient });
+    const router = useRouter();
 
-	const [username, setUsername] = useState('')
-	const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-	const [loggingIn, setLoggingIn] = useState(false)
-	const [error, setError] = useState('')
+    const [loggingIn, setLoggingIn] = useState(false)
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
 
-	async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		setError('');
-		setLoggingIn(true);
-		try {
-			await login({ variables: { username, password } });
-			
+    async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setError('');
+        setSuccess('');
+        setLoggingIn(true);
+        try {
+            await login({ variables: { username, password } });
+            setSuccess('Pieslēgšanās veiksmīga!');
             refreshUser();
-			router.push('/');
-		} catch (error: any) {
+            router.push('/');
+        } catch (error: any) {
             setError(error.message ?? 'Nezināma kļūda')
-		}
+        }
 
-		setLoggingIn(false);
-	}
+        setLoggingIn(false);
+    }
 
-	return (
-		<form onSubmit={handleLogin} className="flex flex-col gap-5">
-			<UsernameInput username={username} setUsername={setUsername} />
-			<PasswordInput password={password} setPassword={setPassword} />
-            <Button type='submit' variant='contained' color='primary'>Pieslēgties</Button>
-			<div className="mt-4">
-				Neesi piereģistrējies? <Link href="/register" className="underline text-blue-500 hover:no-underline">Reģistrēties</Link>
-			</div>
-			{error && <div className="p-2 px-4 bg-red-400 mt-4">
-				{t(error)}
-			</div>}
-		</form>
-	)
+    return (
+        <form onSubmit={handleLogin} className="flex flex-col gap-5">
+            <UsernameInput username={username} setUsername={setUsername} />
+            <PasswordInput password={password} setPassword={setPassword} />
+            <LoadingButton type='submit' variant='contained' color='primary'
+                loading={loggingIn} loadingPosition='end' endIcon={<LoginIcon />}>
+                Pieslēgties
+            </LoadingButton>
+            <div className="mt-4">
+                Neesi piereģistrējies? <Link href="/register" className="underline text-blue-500 hover:no-underline">Reģistrēties</Link>
+            </div>
+            {error &&
+                <Alert severity='error'>
+                    {t(error)}
+                </Alert>
+            }
+            {success &&
+                <Alert severity='success'>
+                    {t(success)}
+                </Alert>
+            }
+        </form>
+    )
 }
