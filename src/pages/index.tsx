392ac35f6bc7b46781e66d0a0b1335aeb396ec52
krisjanisp <krisjanispetrucena@gmail.com>
2023-10-08 13:05:49 +0300

    containerize to reduce width, rework student section in landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,61 +13,11 @@ export default function Home() {
     return (
         <>
             <FirstNavBar/>
-            <div className={"flex justify-center"}>
-                <div className={"w-full relative"}>
-                    <Image src={Hills} alt={"Hills - background image"} style={{
-                        width: '100%',
-                        height: 'auto',
-                        maxHeight: '600px',
-                        overflow: 'hidden',
-                        objectFit: 'cover'
-                    }}/>
-                    <div className={"absolute w-full h-full flex justify-around items-center top-0"}>
-                        <div className={"bg-white p-10 rounded-lg text-3xl text-center flex flex-col gap-5"}>
-                            <div className={"max-w-[14em]"}>
-                                Jauna <span className={"text-green-69 font-medium"}>Pasaule</span> informātikas un
-                                matemātikas
-                                cienītājiem
-                            </div>
-                            <div>
-                            <span className={"bg-blue-69 text-white font-semibold text-xl p-2 px-12 rounded-lg"}>
-                                Ieiet portālā
-                            </span>
-                            </div>
-                        </div>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-
+            <div className={"container m-auto mt-6"}>
+                <Hero/>
             </div>
-            <div className={"px-10 mt-5 flex justify-center"}>
-                <div className={"w-full relative"}>
-                    <div className={"columns-2 flex justify-center items-center gap-5 transform -scale-x-69"}>
-                        <Image src={LP_2} alt={"Programming student"} style={{
-                            width: '25%',
-                            height: 'auto',
-                            maxHeight: '300px',
-                            overflow: 'hidden',
-                            objectFit: 'cover',
-                        }} className={"rounded-lg"}/>
-                        <div className={"bg-white p-5 rounded-lg flex flex-col gap-5 columns-1 w-1/2"}>
-                            <div className={"font-semibold text-lg text-center"}>Skolēniem un studentiem</div>
-                            <ul className={"list-disc text-left text-base"}>
-                                <li>Apgūsti jaunas zināšanas!</li>
-                                <li>Cīnies ar aizraujošiem uzdevumiem!</li>
-                                <li>Seko līdzi savai izaugsmei!</li>
-                            </ul>
-                            <span
-                                className={"bg-green-69 text-white font-semibold text-base text-center p-2 px-4 rounded-lg"}>
-                                Skatīt uzdevumus
-                            </span>
-                        </div>
-                    </div>
-
-                </div>
+            <div className={"container m-auto mt-6"}>
+                <StudentSection/>
             </div>
             <div className={"px-10 mt-5 flex justify-center"}>
                 <div className={"w-full relative"}>
@@ -126,4 +76,61 @@ export default function Home() {
             <Footer/>
         </>
     )
+}
+
+function Hero() {
+    return (<div className={"flex justify-center"}>
+        <div className={"w-full relative"}>
+            <Image src={Hills} alt={"Hills - background image"} style={{
+                width: '100%',
+                height: 'auto',
+                maxHeight: '600px',
+                overflow: 'hidden',
+                objectFit: 'cover'
+            }}/>
+            <div className={"absolute w-full h-full flex justify-around items-center top-0"}>
+                <div className={"bg-white p-10 rounded-lg text-3xl text-center flex flex-col gap-5"}>
+                    <div className={"max-w-[14em]"}>
+                        Jauna <span className={"text-green-69 font-medium"}>Pasaule</span> informātikas un
+                        matemātikas
+                        cienītājiem
+                    </div>
+                    <div>
+                            <span className={"bg-blue-69 text-white font-semibold text-xl p-2 px-12 rounded-lg"}>
+                                Ieiet portālā
+                            </span>
+                    </div>
+                </div>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>)
+}
+
+function StudentSection() {
+    return (<div className={"flex"}>
+        <Image src={LP_2} alt={"Programming student"} style={{
+            width: "400px",
+            height: "auto",
+            overflow: 'hidden',
+            objectFit: 'contain',
+        }}/>
+
+        <div className={"bg-white flex-grow p-6"}>
+            <div className={"flex flex-col gap-5 justify-between h-full max-w-md m-auto"}>
+                <h3 className={"font-semibold text-3xl text-center m-0"}>Skolēniem & Studentiem</h3>
+                <ul className={"list-disc text-left text-2xl flex-grow flex flex-col justify-around"}>
+                    <li>Apgūsti jaunas zināšanas!</li>
+                    <li>Cīnies ar aizraujošiem uzdevumiem!</li>
+                    <li>Seko līdzi savai izaugsmei!</li>
+                </ul>
+                <span className={"bg-green-69 text-white font-semibold text-base text-center p-2 px-4 rounded-lg"}>
+                    Skatīt uzdevumus
+                </span>
+            </div>
+        </div>
+    </div>)
 }
