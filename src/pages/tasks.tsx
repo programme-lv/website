d076a15f7cfd4179ad564c5ecf782a1d0c9ca75d
krisjanisp <krisjanispetrucena@gmail.com>
2023-10-18 22:30:28 +0300

    i did something. dont remember

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,26 +1,23 @@
-import { gql, useQuery } from '@apollo/client'
+import {gql, useQuery} from '@apollo/client'
 import apolloClient from '@/lib/apolloClient'
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import NavigationBar from '@/components/NavigationBar'
-import JoyButton from '@mui/joy/Button'
 import Task from '@/types/task'
 import CreateTaskDialog from '@/components/CreateTaskModal'
 import Link from 'next/link'
+import {Chip} from "@mui/joy";
 
 export default function Tasks() {
     const [createTaskDialogOpened, setCreateTaskDialogOpened] = useState<boolean>(false)
 
     return (
         <>
-            <CreateTaskDialog open={createTaskDialogOpened} handleClose={() => setCreateTaskDialogOpened(false)} />
-            <NavigationBar active='tasks' />
+            <CreateTaskDialog open={createTaskDialogOpened} handleClose={() => setCreateTaskDialogOpened(false)}/>
+            <NavigationBar active='tasks'/>
             <main className="container m-auto">
-                <div className="w-full my-5">
-                    <TaskTable />
-                </div>
-                <div className="flex justify-end">
-                <JoyButton onClick={() => setCreateTaskDialogOpened(true)} className='font-normal'>Izveidot</JoyButton>
-                </div>
+
+                <TaskDisplay code={"summa"} name={"Summa"} description={"Saskaiti divus skaitļus A un B."}
+                             tags={["matemātika", "pilnā pārlase"]} difficulty={"easy"}/>
             </main>
         </>
     )
@@ -37,50 +34,28 @@ query ListTasks {
 }
 `
 
-function TaskTable() { // TODO: convert to server side rendering
-    const { loading, error, data } = useQuery(GET_TASKS, { client: apolloClient })
-    const [tasks, setTasks] = useState<Task[]>([])
-    useEffect(() => { if (data) setTasks(data.listTasks) }, [data])
-
-
-    if (loading) return <p>ielādē uzdevumus</p>
-    if (error) return <p>kļūda: {error.message}</p>
-
-
-    return (
-        <table className="proglvtable text-sm">
-            <thead>
-                <tr>
-                    <th className="w-[10em] text-center capitalize">ID</th>
-                    <th className="text-left capitalize">uzdevums</th>
-                    <th className="w-[5em] text-center capitalize">iesūtījumi</th>
-                    <th className="w-[5em] text-center capitalize">atrisinājuši</th>
-                </tr>
-            </thead>
-            <tbody>
-                {tasks.map((task: Task) => <TaskTableRow task={task} />)}
-            </tbody>
-        </table>
-    )
+interface TaskDisplayProps {
+    code: string;
+    name: string;
+    description: string;
+    tags: string[];
+    difficulty: "easy" | "medium" | "hard";
 }
 
-function TaskTableRow({ task }: { task: Task }) {
-    const tags = ['matemātika', 'skaitļu teorija', 'pilnā pārlase'];
+function TaskDisplay(props: TaskDisplayProps) {
     return (
-        <tr key={task.id}>
-            <td className="text-center">{task.code}</td>
-            <td>
-                <div className="flex justify-between">
-                    <Link href={`tasks/${task.code}`} className="no-underline hover:underline">
-                        <span className="text-blue-69">{task.name}</span>
-                    </Link>
-                    <div className="flex gap-1 text-sm font-normal text-gray-500">
-                        {tags.join(', ')}
-                    </div>
+        <div className="flex flex-col rounded-lg p-5 bg-white">
+            <h3 className="text-xl font-semibold my-0">{props.name}</h3>
+            <p className="text-gray-500">{props.description}</p>
+            <div className={"w-full flex justify-between"}>
+                <div className="flex flex-row flex-wrap gap-2">
+                    {props.tags.map(tag => (
+                        <Chip key={tag} variant="outlined" size="md">{tag}</Chip>
+                    ))}
                 </div>
-            </td>
-            <td className="text-center">0</td>
-            <td className="text-center">0</td>
-        </tr>
+                <Chip variant="solid" color={"success"} size="md">{props.difficulty}</Chip>
+            </div>
+        </div>
     )
+
 }
