00ba526e27bc8e0ccea1b5490e1c41575d1cd5d1
krisjanisp <krisjanispetrucena@gmail.com>
2023-11-22 05:18:22 +0200

    upgrade SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import {
     THEME_ID as MATERIAL_THEME_ID,
 } from '@mui/material/styles';
 import NextNProgress from 'nextjs-progressbar';
+import Head from 'next/head';
 
 const inter = Inter({subsets: ['latin']})
 const cache = createCache({
@@ -26,6 +27,23 @@ export default function App({Component, pageProps}: AppProps) {
 
     return (
         <CacheProvider value={cache}>
+            <Head>
+                <title>programme.lv</title>
+                <meta name="description" content="Programme.lv - Latvijas kodēšanas mācību platforma skolēniem un studentiem. Mācību materiāls un praktiski uzdevumi." />
+                <meta name="keywords" content="kodēšana, programmēšana, mācības, izglītība, Latvija, skolēni, studenti, skolotāji, atvērtā koda, programmēšanas izaicinājumi" />
+                <link rel="canonical" href="https://programme.lv" />
+
+                {/* Open Graph tags */}
+                <meta property="og:title" content="Programme.lv - Kodēšanas Mācību Platforma Latvijā" />
+                <meta property="og:description" content="Programme.lv ir kodēšanas mācību platforma skolēniem un studentiem Latvijā. Uzlabojiet savas prasmes ar mūsu bagātīgo materiālu un praktiskajiem uzdevumiem." />
+                <meta property="og:url" content="https://www.programme.lv" />
+                <meta property="og:type" content="website" />
+
+                {/* Additional tags for enhanced SEO */}
+                <meta name="robots" content="index, follow" />
+                <meta name="author" content="Krišjānis Petručeņa" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <StyledEngineProvider injectFirst>
 
                 <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
