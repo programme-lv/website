80c4d33ea550b0451be917737647135f3b80299c
krisjanisp <krisjanispetrucena@gmail.com>
2023-12-22 16:40:57 +0200

    redirect after logging out

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { mutate } from "swr";
 import { gql,useMutation } from "@apollo/client";
 import apolloClient from '@/lib/apolloClient';
+import { useRouter } from "next/router";
 
 const LOGOUT_MUTATION = gql`
 mutation Logout {
@@ -11,9 +12,11 @@ mutation Logout {
 
 export default function LogOutButton() {
     const [logout] = useMutation(LOGOUT_MUTATION, { client: apolloClient });
+    const router = useRouter();
 
     async function handleLogout() {
         await logout();
+        await router.push('/');
         await mutate('whoami');
     }
 
