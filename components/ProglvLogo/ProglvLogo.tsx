49a47c108089c727ebf25beec42f24360bbdd949
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-21 17:43:21 +0000

    link logo to landing page

--- components/ProglvLogo/ProglvLogo.tsx
+++ components/ProglvLogo/ProglvLogo.tsx
@@ -1,9 +1,12 @@
 import Logo from '@/public/logo.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function ProglvLogo(props: { scale: number }) {
     const { scale } = props
     return (
+        <Link href="/" style={{padding:0, margin:0, display: 'flex'}}>
         <Image src={Logo} alt="Proglv logo" width={360 * scale} />
+        </Link>
     )
 }
