c6acee79b9f66cddd2fb30ec730e08e1522f019a
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-21 10:25:25 +0000

    add a loading bar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/Header/Header';
 import { NavbarMinimal } from '@/components/NavbarMinimal/NavbarMinimal';
 import { AppShell, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import NextTopLoader from 'nextjs-toploader';
 
 export default function DashboardLayout({ children }: { children: any }) {
   const [opened, { toggle }] = useDisclosure();
@@ -26,7 +27,10 @@ export default function DashboardLayout({ children }: { children: any }) {
         <NavbarMinimal/>
       </AppShell.Navbar>
 
-      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Main>
+        <NextTopLoader />
+        {children}
+      </AppShell.Main>
     </AppShell>
   );
 }
