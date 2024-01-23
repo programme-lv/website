a1267bdc90b345018dfbc110c93f8571e82b791d
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-23 09:40:42 +0000

    remove rick roll

--- app/constructor/main/page.tsx
+++ app/constructor/main/page.tsx
@@ -4,7 +4,7 @@ export default function TaskConstructor() {
     return (
         <Container bg="white">
             <h3>Under construction</h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/eMffCtOeigI?si=HQXucL1IQYZIx0X0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/eMffCtOeigI?si=HQXucL1IQYZIx0X0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
         </Container>
     );
 }
