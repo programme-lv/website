68e3e308ad3b39b56d5c74d9ca48a9ba7f373ac3
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-23 19:23:15 +0000

    rendering math

--- app/tasks/view/[code]/page.tsx
+++ app/tasks/view/[code]/page.tsx
@@ -1,10 +1,11 @@
+import renderMD from "@/lib/render"
 import queryTaskDescription from "./queryTaskDesc"
+import 'katex/dist/katex.min.css'
 
 export default async function TaskView(props: any) {
     const task = await queryTaskDescription(props.params.code)
     return (
-        <div>
-            {task.description.story}
+        <div dangerouslySetInnerHTML={{__html:renderMD(task.description.story)}}>
         </div>
     )
 }
