32cefe1595662cbbbf58d24c2aa3d336a199a369
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-24 12:43:37 +0000

    resizable editor

--- app/tasks/view/[code]/RightSide.tsx
+++ app/tasks/view/[code]/RightSide.tsx
@@ -5,7 +5,7 @@ import { Resizable } from "re-resizable";
 export default function RightSide() {
     return (
 
-        <Resizable enable={{ left: true }} defaultSize={{ width: "40%", height: 'auto' }}>
+        <Resizable enable={{ left: true }} defaultSize={{ width: "40%", height: '100%' }}>
         <Stack c={"blue"} bg="blue" h="100%" w="100%">
             12312
         </Stack>
--- app/tasks/view/[code]/page.tsx
+++ app/tasks/view/[code]/page.tsx
@@ -1,6 +1,6 @@
 import queryTaskDescription from "./queryTaskDesc"
 import { TaskTabs } from "./TaskTabs"
-import { Group, Stack } from "@mantine/core"
+import { Flex, Group, Stack } from "@mantine/core"
 import RightSide from "./RightSide"
 
 type Examples = { id: string, input: string, answer: string }[]
@@ -22,13 +22,13 @@ export type Task = {
 export default async function TaskView(props: any) {
     let task = await queryTaskDescription(props.params.code)
     return (
-        <Group grow w={"100%"}>
-            <Stack h="100%">
+        <Flex w={"100%"}>
+            <Stack h="100%" style={{flexGrow:1}}>
                 <h1>Uzdevums "{task.name}"</h1>
                 <TaskTabs task={task as Task}/>
             </Stack>
             <RightSide/>
-        </Group>
+        </Flex>
     )
 }
 
