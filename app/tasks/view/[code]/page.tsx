4694ce9bd2fc00243e12fac0351baac793a74bde
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-25 16:41:01 +0000

    use newest data

--- app/tasks/view/[code]/page.tsx
+++ app/tasks/view/[code]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import queryTaskDescription from "./quries/queryTaskDesc"
 import { ClientTaskView } from "./components/ClientTaskView"
 
