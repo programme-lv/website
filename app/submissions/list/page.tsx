87fea8766bd39911ab1bb654601a7d2fb8217e70
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-25 18:16:01 +0000

    fix rendering

--- app/submissions/list/page.tsx
+++ app/submissions/list/page.tsx
@@ -1,14 +1,12 @@
-'use server';
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 import queryPublicSubmissions from "./queries/querySubmissions";
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
 
 export default async function Submissions() {
 
     const submissions = await queryPublicSubmissions();
-    
     return (
         <>
         Submissions
