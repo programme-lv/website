a4629d9cbe1fa529e950e886bae72dc6ffbf89a5
krisjanisp <krisjanispetrucena@gmail.com>
2024-01-31 20:55:16 +0000

    update hero

--- components/HeroContentLeft/HeroContentLeft.tsx
+++ components/HeroContentLeft/HeroContentLeft.tsx
@@ -6,18 +6,15 @@ export function HeroContentLeft() {
     <div className={classes.hero}>
       <Overlay
         gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-        opacity={1}
+        opacity={0.7}
         zIndex={0}
       />
       <Container className={classes.container} size="md">
-        <Title className={classes.title}>A fully featured React components library</Title>
-        <Text className={classes.description} size="xl" mt="xl">
-          Build fully functional accessible web applications faster than ever – Mantine includes
-          more than 120 customizable components and hooks to cover you in any situation
-        </Text>
-
-        <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
-          Get started
+        <Title className={classes.title}>
+          Jauna Pasaule informātikas un matemātikas cienītājiem
+        </Title>
+        <Button size="xl" radius="md" className={classes.control}>
+          Ieiet portālā
         </Button>
       </Container>
     </div>
