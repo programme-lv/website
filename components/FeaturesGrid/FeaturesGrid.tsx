970ee3fa91e6c4142f35dd551f1ac0d44e041683
krisjanisp <krisjanispetrucena@gmail.com>
2024-03-21 17:21:42 +0200

    change feature icon color to darker

--- components/FeaturesGrid/FeaturesGrid.tsx
+++ components/FeaturesGrid/FeaturesGrid.tsx
@@ -62,7 +62,7 @@ interface FeatureProps {
 export function Feature({ icon: Icon, title, description }: FeatureProps) {
   return (
     <div>
-      <ThemeIcon variant="light" size={40} radius={40}>
+      <ThemeIcon size={40} radius={40}>
         <Icon style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
       </ThemeIcon>
       <Text mt="sm" mb={7}>
