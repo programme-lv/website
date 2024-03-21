e9381e79836821b48bb5cef365779a015d873901
krisjanisp <krisjanispetrucena@gmail.com>
2024-03-21 17:19:19 +0200

    override primary color to navy

--- theme.ts
+++ theme.ts
@@ -3,5 +3,21 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  colors: {
+    'navy': [
+      '#b2c3dd',
+      '#99b0d2',
+      '#819cc6',
+      '#6989ba',
+      '#5176ae',
+      '#3a63a2',
+      '#215096',
+      '#023d89',
+      '#013476',
+      '#012b64',
+      '#012253',
+      '#001a42',
+    ],
+  },
+  primaryColor: 'navy'
 });
