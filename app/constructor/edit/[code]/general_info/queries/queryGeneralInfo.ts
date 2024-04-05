1f79b0cf022305920a0183d3bf20eb97d5e9f711
krisjanisp <krisjanispetrucena@gmail.com>
2024-04-05 18:04:01 +0300

    finalize removing traces of id

--- app/constructor/edit/[code]/general_info/queries/queryGeneralInfo.ts
+++ app/constructor/edit/[code]/general_info/queries/queryGeneralInfo.ts
@@ -8,7 +8,6 @@ query GetCurrentTaskVersionGeneralInfo($code: String!) {
     getCurrentTaskVersionByCode(
         code: $code
     ) {
-        id
         name
         code
         description {
