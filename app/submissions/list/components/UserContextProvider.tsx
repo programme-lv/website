51c7351e1bea2251b7ce48132722eaaf8dc8802d
krisjanisp <krisjanispetrucena@gmail.com>
2024-04-25 21:52:53 +0300

    fix duplication of shell

--- app/submissions/list/components/UserContextProvider.tsx
+++ app/submissions/list/components/UserContextProvider.tsx
@@ -5,10 +5,7 @@ import { AuthContext } from "@/lib/AuthContext";
 export default function UserContextProvider({ user, children }: {user: any,children: any}) {
 	return (
 		<AuthContext.Provider value={{ user: user }}>
-            <ProglvShell activePage="submissions" breadcrumbs={
-                [{ title: "Iesūtījumi", href: "/submissions/list" },]}>
-                {children}
-            </ProglvShell>
+            {children}
 		</AuthContext.Provider>
 	);
 
