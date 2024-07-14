2cee3f949236bafe2cf7588c4dc365bdf542e44a
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-14 19:25:54 +0300

    Added Next.js router and removed unused JWT function import.

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { removeJwt } from "@/api/auth";
 
 export default function StatusPage() {
   const [jwt, setJwt] = useState<string | null>(null);
