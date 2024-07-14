b71afa94765792ffebc57e6db587591dee032152
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-14 15:34:42 +0300

    Updated react-query imports to use '@tanstack/react-query' instead of 'react-query'.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { loginUser } from '@/api/auth';
 import AuthCardWithBG from "@/components/AuthCardWithBG/AuthCardWithBG";
