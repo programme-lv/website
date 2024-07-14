07c9087d6021d1c88591e23fffe4e3dfa3dd400d
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-14 14:42:27 +0300

    delete useless stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import { Navbar } from "@/components/navbar";
 
 export default function Home() {
