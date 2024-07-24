32fd5b3d4119999954585e70272ceef06b70c95b
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-24 22:08:50 +0300

    fix errors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,9 @@
 import {
   Navbar as NextUINavbar,
   NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
   NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
-import clsx from "clsx";
-
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
 
 import LogoWhite from "@/public/logo_white.png";
 import Logo from "@/public/logo.png";
