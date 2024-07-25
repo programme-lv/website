ea1202b0fcf83878c00936c121202ab0abbf13c5
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-25 10:57:04 +0300

    better task card

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,19 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuery } from 'react-query';
 import { listTasks } from "@/lib/tasks";
-import { Button, Card, CardBody } from "@nextui-org/react";
+import TaskCard from "@/components/TaskCard/TaskCard";
 import Alert from "@/components/Alert";
-
-type Task = {
-  published_task_id: string;
-  task_full_name: string;
-  memory_limit_megabytes: number;
-  cpu_time_limit_seconds: number;
-  origin_olympiad: string;
-  lv_pdf_statement_sha: string;
-};
 
 export default function TaskList() {
   const { data, error, isLoading } = useQuery('tasks', listTasks);
@@ -23,23 +14,15 @@ export default function TaskList() {
   }
 
   if (error) {
-    return <Alert message="Failed to load tasks" type="error" onClose={() => null}/>;
+    return <Alert message="Failed to load tasks" type="error" onClose={() => null} />;
   }
 
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-2xl font-bold mb-4">Task List</h1>
       {data && data.length > 0 ? (
-        data.map((task: Task) => (
-          <Card key={task.published_task_id} className="mb-4">
-            <CardBody>
-              <p className="">{task.task_full_name}</p>
-              <p>ID: {task.published_task_id}</p>
-              <p>Memory Limit: {task.memory_limit_megabytes} MB</p>
-              <p>CPU Time Limit: {task.cpu_time_limit_seconds} seconds</p>
-              <p>Origin Olympiad: {task.origin_olympiad}</p>
-            </CardBody>
-          </Card>
+        data.map((task) => (
+          <TaskCard key={task.published_task_id} task={task} />
         ))
       ) : (
         <p>No tasks found</p>
---
+++ components/TaskCard/TaskCard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import type { CardProps } from "@nextui-org/react";
+import React from "react";
+import { Button, Card, Image, CardBody, Chip } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+
+type Task = {
+    published_task_id: string;
+    task_full_name: string;
+    memory_limit_megabytes: number;
+    cpu_time_limit_seconds: number;
+    origin_olympiad: string;
+    lv_pdf_statement_sha: string;
+};
+
+type TaskCardProps = CardProps & {
+    task: Task;
+};
+
+const TaskCard: React.FC<TaskCardProps> = ({ task, ...props }) => {
+    return (
+        <Card className="w-full max-w-[520px] mb-4" {...props} isHoverable>
+            <CardBody className="flex flex-row flex-wrap p-3 sm:flex-nowrap">
+                <Image
+                    removeWrapper
+                    alt={task.task_full_name}
+                    className="h-auto w-full flex-none object-cover object-top md:w-32"
+                    src="https://media.discordapp.net/attachments/1097261687612117084/1265767455834247168/krisjanisp_Once_upon_a_time_in_the_bustling_town_of_Algorithmia_3a71e867-9eff-46e0-ba8b-2ac08a05b378.png?ex=66a2b54e&is=66a163ce&hm=05713bc76bb639e1039fb4e4195787b236f7a91b0920d86647419aaec60bba23&=&format=webp&quality=lossless&width=437&height=437"
+                />
+                <div className="px-4 py-2">
+                    <div className="flex justify-between w-full items-end">
+                    <h3 className="text-large font-medium">{task.task_full_name}</h3>
+                    <Chip color="success" size="sm" variant="flat">ļoti viegls</Chip>
+                    </div>
+                    <div className="flex flex-col gap-3 pt-2 text-small text-default-400">
+                        {/* <p>ID: {task.published_task_id}</p> */}
+                        <p>Krišjānis ir uzkonstruējis kvadrātveida putekļsūcēju (saīsināti – KP), kas ir neaizstājams
+                            palīgs viņa darbnīcas uzkopšanā. KP atmiņā ... </p>
+                        {/* <p>Memory Limit: {task.memory_limit_megabytes} MB</p> */}
+                        {/* <p>CPU Time Limit: {task.cpu_time_limit_seconds} seconds</p>
+            <p>Origin Olympiad: {task.origin_olympiad}</p> */}
+                    </div>
+                    <div className="flex justify-start">
+                        <div className="bottom-1 right-1 w-16">
+                            <Image
+                                src="https://lio.lv/LIO_logo_jaunais3.png"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    );
+};
+
+export default TaskCard;
