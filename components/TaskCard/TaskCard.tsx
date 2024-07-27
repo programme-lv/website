c90a551a9a0b97293e6e967278b219b0b32011c9
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-27 14:08:51 +0300

    pretty origin text

--- components/TaskCard/TaskCard.tsx
+++ components/TaskCard/TaskCard.tsx
@@ -2,8 +2,10 @@
 
 import type { CardProps } from "@nextui-org/react";
 import React from "react";
-import { Button, Card, Image, CardBody, Chip } from "@nextui-org/react";
+import { Button, Card, Image, CardBody, Chip, Tooltip } from "@nextui-org/react";
 import { InlineMath } from "react-katex";
+import { Icon } from '@iconify/react';
+import accountGroup from '@iconify-icons/mdi/account-group';
 import 'katex/dist/katex.min.css'; // Import KaTeX CSS for styling
 
 type Task = {
@@ -16,6 +18,7 @@ type Task = {
     difficulty_rating: 1 | 2 | 3 | 4 | 5;
     illustration_img_url?: string;
     default_md_statement?: MarkdownStatement;
+    solved_count?: number; // Added this for demonstration
 };
 
 type MarkdownStatement = {
@@ -32,6 +35,8 @@ type TaskCardProps = CardProps & {
 
 const TaskCard: React.FC<TaskCardProps> = ({ task, ...props }) => {
     console.log(task);
+
+    task.solved_count = 69;
 
     // Helper function to render story with inline KaTeX
     const renderStory = (story: string) => {
@@ -57,35 +62,53 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, ...props }) => {
 
     return (
         <Card className="w-full max-w-[600px] mb-4" {...props} isHoverable>
-            <CardBody className="flex flex-row flex-wrap p-3 sm:flex-nowrap overflow-y-hidden">
-                <div className="max-w-36 flex">
-                    {task.illustration_img_url &&
-                        <Image
-                            alt={task.task_full_name}
-                            className="h-full flex-none object-center"
-                            src={task.illustration_img_url}
-                        />
-                    }
-                </div>
-                <div className="flex flex-col justify-between px-4 py-2 w-full">
-                    <div>
-                        <div className="flex justify-between w-full items-end">
-                            <h3 className="text-large font-medium">{task.task_full_name}</h3>
-                            {difficultyChips[task.difficulty_rating]}
-                        </div>
-                        <div className="py-2">
-                        {task.default_md_statement && (
-                            <div className="text-small text-default-400 line-clamp-3">
-                                {renderStory(task.default_md_statement.story)}
-                            </div>
-                        )}
-                        </div>
-                    </div>
-                    <div className="flex justify-start py-1">
-                        <div className="w-16">
+            <CardBody className="flex flex-col p-4 sm:flex-nowrap overflow-y-hidden">
+                <div className="flex flex-row flex-wrap sm:flex-nowrap">
+                    <div className="max-w-36 flex">
+                        {task.illustration_img_url &&
                             <Image
-                                src="https://lio.lv/LIO_logo_jaunais3.png"
+                                alt={task.task_full_name}
+                                className="h-full flex-none object-center"
+                                src={task.illustration_img_url}
                             />
+                        }
+                    </div>
+                    <div className="flex flex-col justify-between ps-4 py-2 w-full">
+                        <div>
+                            <div className="flex justify-between w-full items-end">
+                                <h3 className="text-large font-medium">{task.task_full_name}</h3>
+                                {difficultyChips[task.difficulty_rating]}
+                            </div>
+                            <div className="py-2">
+                                {task.default_md_statement && (
+                                    <div className="text-small text-default-400 line-clamp-3">
+                                        {renderStory(task.default_md_statement.story)}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between py-1">
+                            <div className="flex justify-between max-w-72">
+                                <div className="w-16 min-w-16">
+                                    <Image
+                                        src="https://lio.lv/LIO_logo_jaunais3.png"
+                                    />
+                                </div>
+                                <div className="text-tiny text-default-700 py-1 ms-1">
+                                    Uzdevums parādījās Latvijas 37. informātikas olimpiādes (<span className="text-default-900">2024</span>. gads) <span className="text-default-900">skolas</span> kārtā.
+                                </div>
+                            </div>
+                            {task.solved_count !== undefined && (
+                                <div className="inline-flex items-end space-x-2">
+                                    <Tooltip content="lietotāju skaits, kuri atrisināja šo uzdevumu" delay={500}>
+                                        <div className="inline-flex items-end space-x-2">
+                                            <Icon icon={accountGroup} className="text-default-500 text-small" width="24" height="24" />
+                                            <span className="text-default-500 text-small">{task.solved_count}</span>
+                                        </div>
+                                    </Tooltip>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
