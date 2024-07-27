62829d03f0b7629d3a2042a54a09974fba7d9f23
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-27 15:38:28 +0300

    link to task itself

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -5,6 +5,7 @@ import { useQuery } from 'react-query';
 import { listTasks } from "@/lib/tasks";
 import TaskCard from "@/components/TaskCard/TaskCard";
 import Alert from "@/components/Alert";
+import Link from "next/link";
 
 export default function TaskList() {
   const { data, error, isLoading } = useQuery('tasks', listTasks);
@@ -18,11 +19,13 @@ export default function TaskList() {
   }
 
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="flex flex-col items-center p-4 gap-3">
       <h1 className="text-2xl font-bold mb-4">Task List</h1>
       {data && data.length > 0 ? (
         data.map((task) => (
+          <Link href={`/tasks/${task.published_task_id}`} key={task.published_task_id} className="contents">
           <TaskCard key={task.published_task_id} task={task} />
+          </Link>
         ))
       ) : (
         <p>No tasks found</p>
