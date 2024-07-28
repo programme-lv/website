67dbade21aec2efbea4e68c818296a63529218f5
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-28 13:50:16 +0300

    displaying task information in task view

--- app/tasks/[task_id]/page.tsx
+++ app/tasks/[task_id]/page.tsx
@@ -6,10 +6,10 @@ import { Task, getTaskById } from "@/lib/tasks";
 import Alert from "@/components/Alert";
 import { debounce } from "lodash";
 import { Resizable } from "re-resizable";
-import { IconGripVertical } from "@tabler/icons-react";
+import { IconGripVertical, IconSettings } from "@tabler/icons-react";
 import TaskCard from "@/components/TaskCard/TaskCard";
 import { InlineMath } from "react-katex";
-import { Card, CardBody, CardProps, Chip, Tab, Tabs } from "@nextui-org/react";
+import { Button, Card, CardBody, CardProps, Chip, Divider, Select, SelectItem, Tab, Tabs } from "@nextui-org/react";
 import { Image } from "@nextui-org/react";
 
 export default function TaskDetailsPage() {
@@ -57,14 +57,6 @@ function LeftSide({ task }: { task: Task }) {
 
 	const elementRef = React.useRef<HTMLDivElement>(null);
 
-	// const handleResize = useCallback(
-	//   debounce((e, direction, ref, d) => {
-	//     console.log(ref)
-	//     setPdfWidth(ref.clientWidth);
-	//   }, 500),
-	//   []
-	// );
-
 	const handleResize = useCallback(
 		debounce(() => {
 			if (!elementRef.current) return;
@@ -101,8 +93,10 @@ function LeftSide({ task }: { task: Task }) {
 				>
 					{/* <h1 className="text-2xl font-bold mb-4">{task.task_full_name}</h1> */}
 					<TaskInformation task={task} />
+
+					<Divider className="my-1" />
 					<div className="bg-violet-100 flex-grow w-full"
-					ref={elementRef}>
+						ref={elementRef}>
 						<div
 							style={{ width: pdfWidth, height: pdfHeight }}
 							className="absolute left-0"
@@ -160,85 +154,104 @@ const TaskInformation: React.FC<TaskInformationProps> = ({
 		),
 	};
 
+	const cardRef = useRef<HTMLDivElement>(null);
+	const [layout, setLayout] = useState<"narrow" | "wide">("wide");
+
+	function handleCardResize(cardWidth: number) {
+		if (cardWidth < 500) {
+			setLayout("narrow");
+		} else {
+			setLayout("wide");
+		}
+	}
+
+	useEffect(() => {
+		if (!cardRef.current) return;
+		handleCardResize(cardRef.current.clientWidth);
+		const resizeObserver = new ResizeObserver(() => {
+			if (!cardRef.current) return;
+			handleCardResize(cardRef.current.clientWidth);
+		});
+		resizeObserver.observe(cardRef.current);
+		return () => resizeObserver.disconnect();
+	}, [cardRef]);
+
+	console.log(layout)
+
 	return (
-		<Card className="w-full mb-4" {...props} shadow="none">
+		<Card className="w-full" {...props} shadow="none" radius="none" ref={cardRef}>
 			<CardBody className="flex flex-col p-0 sm:flex-nowrap overflow-y-hidden">
 				<div className="flex flex-row">
 					<div className="h-full flex flex-row flex-wrap sm:flex-nowrap flex-grow">
-						<div className="max-w-40 flex">
-							{task.illustration_img_url && (
-								<Image
-									alt={task.task_full_name}
-									className="h-full flex-none object-cover"
-									src={task.illustration_img_url}
-								/>
-							)}
-						</div>
-						<div className="flex flex-col justify-start ps-4 pt-2 pb-1 w-full">
+						{layout === "wide" && task.illustration_img_url && (<div className="max-w-40 min-w-20 flex">
+							<Image
+								alt={task.task_full_name}
+								className="h-full flex-none object-cover"
+								src={task.illustration_img_url}
+							/>
+						</div>)}
+						<div className="flex flex-col justify-between ps-4 pt-2 pb-1 w-full">
 							<div>
-								<div className="flex justify-between w-full items-end">
-									<h3 className="text-large font-medium">
-										{task.task_full_name}
-									</h3>
-									{difficultyChips[task.difficulty_rating]}
+								<div>
+									<div className="inline-flex gap-x-2 gap-y-1 justify-between items-center flex-wrap">
+										<h3 className="text-large font-medium">
+											{task.task_full_name}
+										</h3>
+										{difficultyChips[task.difficulty_rating]}
+									</div>
+								</div>
+
+								<div className="flex justify-between pt-1 max-w-72">
+									<div className="flex justify-between ">
+										{task.origin_olympiad && task.origin_olympiad === "LIO" && (
+											<div className="w-16 min-w-16">
+												<Image src="https://lio.lv/LIO_logo_jaunais3.png" />
+											</div>
+										)}
+										{task.origin_notes && task.origin_notes["lv"] && (
+											<div className="text-tiny text-default-700 py-1 ms-1">
+												{task.origin_notes["lv"]}
+											</div>
+										)}
+									</div>
+								</div>
+							</div>
+							<div className="flex-grow flex flex-col justify-end items-end ms-3">
+								<div className="grid grid-cols-2 gap-x-2">
+									<span className="text-tiny text-default-700 flex items-end justify-end">izpildes laiks</span>
+									<div className="flex items-end gap-1">
+										<span className="text-small text-default-900">{task.cpu_time_limit_seconds}</span>
+										<span className="text-tiny text-default-800">sek.</span>
+									</div>
+									<span className="text-tiny text-default-700 flex items-end justify-end"> atmiņa</span>
+									<div className="flex items-end gap-1">
+										<span className="text-small text-default-900">{task.memory_limit_megabytes}</span>
+										<span className="text-tiny text-default-800">MB</span>
+									</div>
 								</div>
 							</div>
 
-							<div className="flex justify-between pt-1">
-								<div className="flex justify-between max-w-72">
-									{task.origin_olympiad && task.origin_olympiad === "LIO" && (
-										<div className="w-16 min-w-16">
-											<Image src="https://lio.lv/LIO_logo_jaunais3.png" />
-										</div>
-									)}
-									{task.origin_notes && task.origin_notes["lv"] && (
-										<div className="text-tiny text-default-700 py-1 ms-1">
-											{task.origin_notes["lv"]}
-										</div>
-									)}
+							{/* <div className="flex items-center mt-2 gap-3">
+								<Tabs aria-label="Tab" size="sm">
+									<Tab key="statement" title="Formulējums">
+									</Tab>
+								</Tabs>
+
+								<div className="flex gap-4 items-center">
+									<Button isIconOnly variant="light" aria-label="Take a photo" size="sm">
+										<IconSettings className="text-default-600" />
+									</Button>
 								</div>
-							</div>
-						<Tabs aria-label="Options" size="sm" className="mt-2">
-							<Tab key="photos" title="Formulējums">
-								{/* <Card>
-									<CardBody>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-									</CardBody>
-								</Card> */}
-							</Tab>
-							<Tab key="music" title="Mani iesūtījumi">
-								{/* <Card>
-									<CardBody>
-										Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-									</CardBody>
-								</Card> */}
-							</Tab>
-							<Tab key="music2" title="Visi iesūtījumi">
-								{/* <Card>
-									<CardBody>
-										Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-									</CardBody>
-								</Card> */}
-							</Tab>
-							<Tab key="videos" title="Risinājums">
-								{/* <Card>
-									<CardBody>
-										Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-									</CardBody>
-								</Card> */}
-							</Tab>
-						</Tabs>
+							</div> */}
 						</div>
 					</div>
-					<div className="flex-grow">
-						{/* Izpildes laika ierobežojums: {task.cpu_time_limit_seconds} sekundes
-						Izpildes atmiņas ierobežojums: {task.memory_limit_megabytes} MB */}
-					</div>
+
 				</div>
 			</CardBody>
 		</Card>
 	);
 };
+
 function RightSide() {
 	return (
 		<div className="flex flex-col items-center p-4 gap-3 overflow-hidden flex-grow min-w-10 bg-red-100">
