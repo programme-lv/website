7c8167e9824927c51862a6622fec26a49f54181c
krisjanisp <krisjanispetrucena@gmail.com>
2025-07-07 18:35:22 +0300

    fix compile error

--- app/admin/task-admin-list.tsx
+++ app/admin/task-admin-list.tsx
@@ -1,10 +1,10 @@
 import GenericTable from "@/components/generic-table";
 import GenericButton from "@/components/generic-button";
 import { ApiResponse } from "@/lib/api-response";
-import { Task } from "@/types/task";
+import { Task, TaskPreview } from "@/types/task";
 import Link from "next/link";
 
-export default function TaskAdminList({ tasks }: { tasks: ApiResponse<Task[]> }) {
+export default function TaskAdminList({ tasks }: { tasks: ApiResponse<TaskPreview[]> }) {
     return (
             <div className="p-2 bg-white  max-w-2xl">
                 <GenericTable
@@ -13,18 +13,18 @@ export default function TaskAdminList({ tasks }: { tasks: ApiResponse<Task[]> })
                         {
                             key: "id",
                             header: "Uzdevuma ID",
-                            render: (task) => task.short_task_id
+                            render: (task) => task.short_id
                         },
                         {
                             key: "name",
                             header: "Pilnais nosaukums",
-                            render: (task) => task.task_full_name
+                            render: (task) => task.full_name
                         },
                         {
                             key: "action",
                             header: "Darbība",
                             render: (task) => (
-                                <Link href={`/admin/task/${task.short_task_id}`}>
+                                <Link href={`/admin/task/${task.short_id}`}>
                                     <GenericButton variant="primary" size="sm">
                                         Rediģēt
                                     </GenericButton>
@@ -32,7 +32,7 @@ export default function TaskAdminList({ tasks }: { tasks: ApiResponse<Task[]> })
                             ),
                         }
                     ]}
-                    keyExtractor={(task) => task.short_task_id}
+                    keyExtractor={(task) => task.short_id}
                     className="w-full"
                 />
     </div>
