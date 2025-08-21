a8d39fae99d0d41f9f723cc8974216b3b0eb0df2
krisjanisp <krisjanispetrucena@gmail.com>
2025-08-21 23:07:03 +0300

    server side render user max scores

--- app/(platform)/tasks/page.tsx
+++ app/(platform)/tasks/page.tsx
@@ -6,16 +6,31 @@ import { listTasks } from "@/lib/task/tasks";
 import Layout from "@/components/layout";
 
 import { TaskList } from "./task-list";
+import whoami from "@/lib/user/whoami";
+import { getMaxScorePerTaskServerSide } from "@/lib/subm/list-ss";
+import { MaxScorePerTask } from "@/types/scores";
 
 export default async function TaskListServerComponent() {
   const tasks = await listTasks();
+
+  // Load current user and their per-task scores on the server
+  const me = await whoami();
+  let userMaxScores: MaxScorePerTask | undefined = undefined;
+  const username = me?.data?.username;
+  if (username) {
+    try {
+      userMaxScores = await getMaxScorePerTaskServerSide(username);
+    } catch (e) {
+      // noop: keep undefined on error
+    }
+  }
 
   const breadcrumbs = [{ label: "Uzdevumi", href: "/tasks" }];
 
   return (
     <Layout breadcrumbs={breadcrumbs} active="tasks">
       <div className="px-3">
-        <TaskList tasks={tasks.data ?? []} />
+        <TaskList tasks={tasks.data ?? []} userMaxScores={userMaxScores} />
       </div>
     </Layout>
   );
